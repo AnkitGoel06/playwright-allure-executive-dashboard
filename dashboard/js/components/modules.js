7edfbf3a4e075ac4bc714c02027f3fe18ccb7533
Ankit Goel <ankit.goel-v@adityabirla.com>
2026-07-12 17:27:54 +0530

    changes

--- dashboard/js/components/modules.js
+++ dashboard/js/components/modules.js
@@ -1,8 +1,8 @@
 export function renderModules(data) {
 
-    const tbody = document.getElementById("moduleTable");
+    const container = document.getElementById("moduleList");
 
-    tbody.innerHTML = "";
+    container.innerHTML = "";
 
     data.suites.items.forEach(module => {
 
@@ -10,23 +10,44 @@ export function renderModules(data) {
 
         const passRate = ((stats.passed / stats.total) * 100).toFixed(1);
 
-        tbody.innerHTML += `
-            <tr>
-                <td>${module.name}</td>
-                <td>${stats.total}</td>
-                <td>
-                    <div class="progress-cell">
-                        <div class="progress-bar">
-                            <div class="progress-fill"
-                                 style="width:${passRate}%"></div>
-                        </div>
-                        <span>${passRate}%</span>
-                    </td>
-                <td class="${stats.failed > 0 ? 'failed-text' : 'passed-text'}">
-                    ${stats.failed}
-                </td>
-            </tr>
-        `;
+        container.innerHTML += `
+<div class="module-item">
+
+    <div class="module-name">
+        ${module.name}
+    </div>
+
+    <div class="module-tests">
+        ${stats.total}
+    </div>
+
+    <div class="progress-wrapper">
+
+        <div class="progress-bar">
+
+            <div class="progress-fill"
+                 style="width:${passRate}%">
+            </div>
+
+        </div>
+
+        <span class="progress-value">
+            ${passRate}%
+        </span>
+
+    </div>
+
+    <div>
+
+        <span class="${stats.failed ? 'failed-text' : 'passed-text'}">
+
+            ${stats.failed}
+
+        </span>
+
+    </div>
+
+</div>`;
 
     });
 
